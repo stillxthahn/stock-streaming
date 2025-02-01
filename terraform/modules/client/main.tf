c3428493bdecb4bfd7c3adf4b0ad15b8679ae8b3
Xuan Thanh <lxthanh235@gmail.com>
2025-02-02 00:00:17 +0700

    update: security groups

--- terraform/modules/client/main.tf
+++ terraform/modules/client/main.tf
@@ -3,15 +3,15 @@ resource "aws_security_group" "client-sg" {
   vpc_id = var.vpc_id
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow http" {
-  security_group_id = aws_security_group.client-sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 22
-  ip_protocol       = "ssh"
-  to_port           = 22
-}
+# resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+#   security_group_id = aws_security_group.client-sg.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   from_port         = 22
+#   ip_protocol       = "ssh"
+#   to_port           = 22
+# }
 
-resource "aws_vpc_security_group_ingress_rule" "allow http" {
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   security_group_id = aws_security_group.client-sg.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
